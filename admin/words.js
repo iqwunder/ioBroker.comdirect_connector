c1ff3554be34efb82c451399a9f1bbdd8fd0d897
Steffen <steffen@iq-wunder.de>
2021-07-01 08:31:42 +0200

    Initial commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+"use strict";
+
+systemDictionary = {
+	"comdirect_connector adapter settings": {
+		"en": "Adapter settings for comdirect_connector",
+		"de": "Adaptereinstellungen für comdirect_connector",
+		"ru": "Настройки адаптера для comdirect_connector",
+		"pt": "Configurações do adaptador para comdirect_connector",
+		"nl": "Adapterinstellingen voor comdirect_connector",
+		"fr": "Paramètres d'adaptateur pour comdirect_connector",
+		"it": "Impostazioni dell'adattatore per comdirect_connector",
+		"es": "Ajustes del adaptador para comdirect_connector",
+		"pl": "Ustawienia adaptera dla comdirect_connector",
+		"zh-cn": "comdirect_connector的适配器设置"
+	},
+	"option1": {
+		"en": "option1",
+		"de": "Option 1",
+		"ru": "Опция 1",
+		"pt": "Opção 1",
+		"nl": "Optie 1",
+		"fr": "Option 1",
+		"it": "opzione 1",
+		"es": "Opción 1",
+		"pl": "opcja 1",
+		"zh-cn": "选项1"
+	},
+	"option2": {
+		"en": "option2",
+		"de": "Option 2",
+		"ru": "option2",
+		"pt": "opção 2",
+		"nl": "Optie 2",
+		"fr": "Option 2",
+		"it": "opzione 2",
+		"es": "opcion 2",
+		"pl": "Opcja 2",
+		"zh-cn": "选项2"
+	}
+};
